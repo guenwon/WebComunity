cbe24f1ee24267c9071be01b3880263df7ea508d
guenwon <kgw980318@gmail.com>
2022-03-11 14:27:33 +0900

    first commit

---
+++ src/main/java/com/miniComunity/test/WebComunity/WebComunityApplication.java
@@ -0,0 +1,13 @@
+package com.miniComunity.test.WebComunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebComunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniComunity/test/WebComunity/WebComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniComunity.test.WebComunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebComunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
